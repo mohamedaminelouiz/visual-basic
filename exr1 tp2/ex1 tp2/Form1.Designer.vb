42aa841c49965388e46bc63160339d9261b49a85
mohamedaminelouiz <mohamedamine.louiz@isgb.ucar.tn>
2025-12-29 15:53:40 +0100

    Add files via upload

---
+++ exr1 tp2/ex1 tp2/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        nomtxt = New TextBox()
+        agetxt = New TextBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(85, 83)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(39, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "nom"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(85, 169)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(34, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "age"
+        ' 
+        ' nomtxt
+        ' 
+        nomtxt.Location = New Point(247, 83)
+        nomtxt.Name = "nomtxt"
+        nomtxt.Size = New Size(125, 27)
+        nomtxt.TabIndex = 2
+        ' 
+        ' agetxt
+        ' 
+        agetxt.Location = New Point(247, 169)
+        agetxt.Name = "agetxt"
+        agetxt.Size = New Size(125, 27)
+        agetxt.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(247, 295)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 4
+        Button1.Text = "ajouter"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(434, 450)
+        Controls.Add(Button1)
+        Controls.Add(agetxt)
+        Controls.Add(nomtxt)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents nomtxt As TextBox
+    Friend WithEvents agetxt As TextBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ exr1 tp2/ex1 tp2/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If String.IsNullOrEmpty(nomtxt.Text) Then
+            MessageBox.Show("tapez nom", "error")
+        ElseIf String.IsNullOrEmpty(agetxt.Text) Then
+            MessageBox.Show("tapez votre age", "error")
+        ElseIf CInt(agetxt.text) < 0 Then
+            MessageBox.Show("votre age est positif et un entier", "error")
+        Else
+            MessageBox.Show("bonjour " + nomtxt.Text + ",vouz avez " + agetxt.Text + " ans", "affichage")
+        End If
+    End Sub
+End Class
