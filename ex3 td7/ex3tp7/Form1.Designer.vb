b137c2361bd4924c44fab4a89c672c7717674eb4
mohamedaminelouiz <mohamedamine.louiz@isgb.ucar.tn>
2025-12-29 15:55:26 +0100

    Add files via upload

---
+++ ex3 td7/ex3tp7/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        moy = New TextBox()
+        Label1 = New Label()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' moy
+        ' 
+        moy.Location = New Point(115, 132)
+        moy.Name = "moy"
+        moy.Size = New Size(125, 27)
+        moy.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(139, 75)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(70, 20)
+        Label1.TabIndex = 1
+        Label1.Text = "moyenne"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(126, 188)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 2
+        Button1.Text = "resultat"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(375, 287)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Controls.Add(moy)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents moy As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ ex3 td7/ex3tp7/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer = CInt(moy.Text)
+        Dim aff As String
+        If a >= 10 Then
+            aff = "admis"
+        ElseIf a >= 8 And a < 10 Then
+            aff = "rattrapage"
+        Else
+            aff = "ajourné"
+        End If
+        MessageBox.Show(aff, "votre resultat:")
+    End Sub
+End Class
