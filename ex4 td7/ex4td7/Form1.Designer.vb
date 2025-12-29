3a44be092130f8ed6dd8ccc652252360aa13101f
mohamedaminelouiz <mohamedamine.louiz@isgb.ucar.tn>
2025-12-29 15:56:42 +0100

    Add files via upload

---
+++ ex4 td7/ex4td7/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        txt = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Label3 = New Label()
+        aff = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(12, 40)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(236, 31)
+        Label1.TabIndex = 0
+        Label1.Text = "analyse d'une chaine"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(12, 98)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(215, 31)
+        Label2.TabIndex = 1
+        Label2.Text = "entrez une phrases:"
+        ' 
+        ' txt
+        ' 
+        txt.Location = New Point(276, 102)
+        txt.Name = "txt"
+        txt.Size = New Size(394, 27)
+        txt.TabIndex = 2
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(50, 184)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(145, 44)
+        Button1.TabIndex = 3
+        Button1.Text = "majuscule"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(276, 184)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(145, 44)
+        Button2.TabIndex = 4
+        Button2.Text = "minuscules"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button3.Location = New Point(525, 184)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(145, 44)
+        Button3.TabIndex = 5
+        Button3.Text = "longeur"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button4.Location = New Point(50, 272)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(145, 44)
+        Button4.TabIndex = 6
+        Button4.Text = "remplacer"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button5.Location = New Point(276, 272)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(145, 44)
+        Button5.TabIndex = 7
+        Button5.Text = "decouper"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(12, 366)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(98, 31)
+        Label3.TabIndex = 8
+        Label3.Text = "resultat:"
+        ' 
+        ' aff
+        ' 
+        aff.AutoSize = True
+        aff.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        aff.Location = New Point(150, 366)
+        aff.Name = "aff"
+        aff.Size = New Size(0, 31)
+        aff.TabIndex = 9
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(aff)
+        Controls.Add(Label3)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(txt)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txt As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents aff As Label
+
+End Class
---
+++ ex4 td7/ex4td7/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        aff.Text = txt.Text.ToUpper
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        aff.Text = txt.Text.ToLower
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        aff.Text = txt.Text.Length
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        aff.Text = txt.Text.Replace("ou", "xx")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        aff.Text = txt.Text.Substring(0, 3)
+    End Sub
+End Class
